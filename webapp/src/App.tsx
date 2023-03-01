cfc54176612203674a1937961c2eca4680a618f1
alonso <uo282834@uniovi.es>
2023-03-01 20:15:48 +0100

    Primer ejemplo de un mapa

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,37 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
-import EmailForm from './components/EmailForm';
-import Welcome from './components/Welcome';
-import UserList from './components/UserList';
-import  {getUsers} from './api/api';
-import {User} from './shared/shareddtypes';
+import React, { useEffect,useRef } from 'react';
 import './App.css';
+import { initMap } from './utils/initMap';
 
 function App(): JSX.Element {
 
-  const [users,setUsers] = useState<User[]>([]);
+  const mapRef = useRef<HTMLDivElement>(null);
 
-  const refreshUserList = async () => {
-    setUsers(await getUsers());
-  }
 
-  useEffect(()=>{
-    refreshUserList();
-  },[]);
+  useEffect(() => {
+    if (mapRef.current) {
+        initMap(
+            mapRef.current,
+            [-5.851543817083269,43.3548058269008]
+        )
+    }
+}, []);
 
   return (
-    <>
-      <Container maxWidth="sm">
-        <Welcome message="ASW students"/>
-        <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
-        <EmailForm OnUserListChange={refreshUserList}/>        
-        <UserList users={users}/>
-        <Link href="https://github.com/arquisoft/lomap_0">Source code</Link>
-      </Container>
-    </>
-  );
+  
+  <div ref={mapRef} className='map' />);
 }
 
 export default App;
---
+++ webapp/src/utils/initMap.ts
@@ -0,0 +1,15 @@
+import { Map } from 'mapbox-gl';
+
+export const initMap = (container: HTMLDivElement, coords: [number, number]) => {
+
+    return new Map({
+        container,
+        style: 'mapbox://styles/mapbox/dark-v10',
+        pitchWithRotate: false,
+        center: coords,
+        zoom: 15,
+        accessToken: "pk.eyJ1IjoidW8yODI4MzQiLCJhIjoiY2xlcHp5Z2syMGRteTQ5cDJ2dXltMm5uYSJ9.kTLZTl2_YvQiN79m2kPQ1g",
+        doubleClickZoom: false
+        
+    });
+}
