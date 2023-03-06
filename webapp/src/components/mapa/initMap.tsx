1f9e795c4d3e66a3c716dd6e96ef791e02bcb043
Javier <uo282944@uniovi.es>
2023-03-06 12:27:13 +0100

    Creado componente con mapa y añadido a la ventana mapa closed#42

--- webapp/src/components/mapa/initMap.tsx
+++ webapp/src/components/mapa/initMap.tsx
@@ -1,15 +1,20 @@
+import mapboxgl from 'mapbox-gl';
 import { Map } from 'mapbox-gl';
 
-const initMap = (container: HTMLDivElement, coords: [number, number]) => {
-    return new Map({
+export const initMap = (container: HTMLDivElement, coords: [number, number]) => {
+
+    const mapa = new Map({
         container,
         style: 'mapbox://styles/mapbox/dark-v10',
         pitchWithRotate: false,
         center: coords,
         zoom: 15,
-        accessToken: 'pk.eyJ1IjoidW8yODI5NDQiLCJhIjoiY2xiM3d4MnNwMDJoMDNuczVxbXkzd2R1NyJ9.CoUhyImTRE_3w_zsY0p5Ug',
+        accessToken: "pk.eyJ1IjoidW8yODI4MzQiLCJhIjoiY2xlcHp5Z2syMGRteTQ5cDJ2dXltMm5uYSJ9.kTLZTl2_YvQiN79m2kPQ1g",
         doubleClickZoom: false
+        
     });
+     new mapboxgl.Marker()
+    .setLngLat([-5.851543817083269,43.3548058269008])
+    .addTo(mapa);
+    return mapa;
 }
-
-export default initMap;
--- webapp/src/components/mapa/mapa.tsx
+++ webapp/src/components/mapa/mapa.tsx
@@ -1,21 +1,27 @@
-import React, { useRef, useEffect, useState } from 'react';
-import initMap from './initMap';
+import React, { useEffect,useRef } from 'react';
+import '../../hojasEstilo/mapa.css';
+import { initMap } from './initMap';
 
-function Mapa(){
-    const mapRef = useRef<HTMLDivElement>(null);
+function Mapa(): JSX.Element {
+  const mapRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (mapRef.current) {
-            initMap(
-                mapRef.current,
-                [-100.31019063199852, 25.66901932031443]
-            )
-        }
-    }, []);
 
-    return (
-        <div ref={mapRef} className='map' />
-    )
+  useEffect(() => {
+    if (mapRef.current) {
+        initMap(
+            mapRef.current,
+            [-5.851543817083269,43.3548058269008]
+        )
+    }
+}, []);
+
+  return (
+  <>
+  <div className='contenedor-mapa'>
+    <div ref={mapRef} className='map' />
+  </div>
+  </>
+  );
 }
 
 export default Mapa;
