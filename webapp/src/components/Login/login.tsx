f9550d67c3c65e351dcf5c95c908f1ceaefa1214
Javier <uo282944@uniovi.es>
2023-03-09 00:18:40 +0100

    Creado home con slide de imagenes closed#47

--- webapp/src/components/Login/login.tsx
+++ webapp/src/components/Login/login.tsx
@@ -1,26 +1,32 @@
-import React from "react";
 import '../../hojasEstilo/home.css'
-
-import { SessionProvider, LoginButton } from "@inrupt/solid-ui-react";
+import {Image, LogoutButton,Text,CombinedDataProvider, useSession, SessionProvider, LoginButton } from "@inrupt/solid-ui-react";
+import { Container, FormGroup, TextField, Button, Typography, CardContent, Card, CardActionArea} from "@material-ui/core"
+import React, { useEffect,useRef,useState } from 'react';
+import LoginForm from './LoginForm';
+import ProfileViewer from "./ProfileViewer";
 
 
 function Login() {
-    return (
-      <div className="contenedor_login_principal">
-        <SessionProvider sessionId="log-in">
-            <p>
-                <em>
-                Note:{' '}
-                </em>
-                to test out the Authentication examples, you will need to click the pop-out icon on the top right to open this example in a new tab first.
-            </p>
-            <LoginButton
-                oidcIssuer="https://inrupt.net"
-                redirectUrl="https://solid-ui-react.docs.inrupt.com/iframe.html?viewMode=docs&id=authentication-login-button--with-children&args="
-            />
-        </SessionProvider>
-      </div>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //With this we can control the login status for solid
+  const { session } = useSession();
+
+  //We have logged in
+  session.onLogin(()=>{
+    setIsLoggedIn(true)
+  })
+
+  //We have logged out
+  session.onLogout(()=>{
+    setIsLoggedIn(false)
+  })
+
+  return(
+    <SessionProvider sessionId="log-in-example">
+      {(!isLoggedIn) ? <LoginForm/> : <ProfileViewer/>}
+    </SessionProvider>
+  )
   }
   
   export default Login;
