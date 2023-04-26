2e60fadaa4d2dd69c6818c527d8bba58739835e4
ruben <uo283204@uniovi.es>
2023-04-26 20:34:09 +0200

    TEST

--- webapp/src/components/Login/login.tsx
+++ webapp/src/components/Login/login.tsx
@@ -2,18 +2,9 @@ import '../../hojasEstilo/home.css'
 import { useSession, SessionProvider } from "@inrupt/solid-ui-react";
 import LoginForm from './LoginForm';
 import ProfileViewer from "./ProfileViewer";
-import { Session } from '@inrupt/solid-client-authn-browser';
-
-const setUserSession = (session :Session) => {
-  localStorage.setItem("userSession", JSON.stringify(session));
-};
 
 function Login() {
   const { session } = useSession();
-
-  console.log(session);
-  if(!session.info.isLoggedIn)
-      setUserSession(session)
 
   return(
     <SessionProvider sessionId="">
