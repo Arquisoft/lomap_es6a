629b851c4585351a96f7cea928137f731f01afd8
Javier <uo282944@uniovi.es>
2023-04-26 16:48:25 +0200

    Modificando estilos

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -51,9 +51,6 @@ function App(): JSX.Element {
               <Route  path="/ProfileViewer" element={<ProfileViewer/>}/>
             </Routes>
             </div>
-          <div className='contenedor-footer'>
-            <Footer/>
-          </div>
         </Router>
       </SessionProvider>
     </>
