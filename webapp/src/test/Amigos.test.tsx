4974f473333c151325dab3f632311229c1841a84
UO284622 <UO284622@uniovi.es>
2023-04-27 16:07:58 +0200

    Test para amigos

--- webapp/src/test/Amigos.test.tsx
+++ webapp/src/test/Amigos.test.tsx
@@ -10,7 +10,7 @@ import nav from "../components/fragments/nav";
 import { useSession } from "@inrupt/solid-ui-react";
 import Form from '../components/mapa/form';
 import MapaAmigos from '../components/mapa/mapa';
-import Marker from '../accesoPods/marker';
+import Amigos from '../components/amigos/amigos';
 import {guardarMarcador, guardarMarcadorSinImagen} from "../accesoPods/adaptador";
 //import { Session } from "@inrupt/solid-client-authn-node";
 
@@ -28,52 +28,24 @@ test('prueba login',()=>{
         // 3. Your session should now be logged in, and able to make authenticated requests.
        // session
         //console.log(`Logged in with WebID [${session.info.webId}]`);
-        session.handleIncomingRedirect(window.location.protocol + '//' + window.location.host + "/mapa");
-        var home = render(<MapaAmigos session={session}/>);
+        session.handleIncomingRedirect(window.location.protocol + '//' + window.location.host + "/amigos");
+        var amigos = render(<Amigos/>);
 
-        home.findAllByText("Añadir marcador").then((tmp) => (
+        amigos.findAllByText("Buscar Perfil").then((tmp) => (
             expect(tmp).toBeInTheDocument()
         ));
-        home.findByLabelText("Filtros").then((tmp) => (
+        amigos.findByLabelText("Bienvenido a Amigos").then((tmp) => (
             expect(tmp).toBeInTheDocument()
         ));
-
+        amigos.findByLabelText("Mis amigos").then((tmp) => (
+            expect(tmp).toBeInTheDocument()
+        ));
         }
         else
             fail();
         session.logout();
     });
 })
-
-describe("logedNav",()=>{
-    const session = new Session();
-    session.login({
-      // 2. Use the authenticated credentials to log in the session.
-      clientId: "https://testASW.inrupt.net/profile/card#me",
-      clientSecret: "1234567890ABCabc.",
-      oidcIssuer: "https://inrupt.net",
-      //redirectUrl : "" ,
-      redirectUrl: window.location.protocol + '//' + window.location.host + "/Home",
-
-    }).then(() => {
-      if (session.info.isLoggedIn) {
-        // 3. Your session should now be logged in, and able to make authenticated requests.
-       // session
-       // console.log(`Logged in with WebID [${session.info.webId}]`);
-        session.handleIncomingRedirect(window.location.protocol + '//' + window.location.host + "/Home");
-        test("renders the marker from the database", async () => {
-            guardarMarcadorSinImagen({session}.session, "Marker 1", "a",  42.7128, -74.006, "Restaurante");
-            render(
-                <MapaAmigos session={session}/>
-            );
-            const marker1 = await screen.getByAltText("Marker");
-            expect(marker1).toBeInTheDocument();
-        })
-        session.logout();
-      }
-    });
-});
-
 
 afterAll(async () => {
     await getDefaultSession().logout();
