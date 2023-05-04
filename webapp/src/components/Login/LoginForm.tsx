422265b82e85881bfe43384e73b16ec65a240a65
Javier <uo282944@uniovi.es>
2023-05-04 20:38:17 +0200

    Prueba de despliegue 7

--- webapp/src/components/Login/LoginForm.tsx
+++ webapp/src/components/Login/LoginForm.tsx
@@ -57,7 +57,7 @@ return (
                 value={idp}
                 InputProps={{
                   endAdornment: (
-                    <LoginButton  oidcIssuer={idp} redirectUrl={"https://uo282944.github.io/Login"}>
+                    <LoginButton  oidcIssuer={idp} redirectUrl={"https://uo282944.github.io/lomap_6a_despliegue/"}>
                       <Button variant="contained" color="primary" aria-label="loginButton">
                         Login
                       </Button>
